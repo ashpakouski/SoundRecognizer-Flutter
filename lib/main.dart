09c04d1f587dbb876ded742a53078c49ff73aac7
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2022-04-24 13:28:12 +0300

    Add sound visualizer widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sound_recognizer/utils/sound_player.dart';
 import 'package:sound_recognizer/utils/sound_recorder.dart';
+import 'component/audio_visualizer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +17,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final SoundRecorder _soundRecorder = SoundRecorder();
   final SoundPlayer _soundPlayer = SoundPlayer();
+
+  final List<int> currentAudioData = [];
+
+  int lastFrame = 0;
+  int thisFrame = DateTime.now().millisecondsSinceEpoch;
 
   @override
   Widget build(BuildContext context) {
@@ -34,14 +40,41 @@ class _MyAppState extends State<MyApp> {
                   await _soundRecorder.open();
 
                   List<int> soundValues = await _soundRecorder.recordSound(
-                    duration: const Duration(seconds: 10),
+                    duration: const Duration(seconds: 12),
+                    onNewData: (newData) {
+                      thisFrame = DateTime.now().millisecondsSinceEpoch;
+
+                      if (thisFrame - lastFrame > 1000 / 15) {
+                        currentAudioData.clear();
+
+                        setState(() {
+                          currentAudioData.addAll(newData);
+                        });
+
+                        lastFrame = thisFrame;
+                      }
+                    },
                   );
 
                   _soundPlayer.play(soundValues);
                 },
                 child: const Text("Start recording"),
               ),
-              const Text("Tap to start recording"),
+              SizedBox(
+                height: 100,
+                child: AudioVisualizer(
+                  soundValues: currentAudioData,
+                  barsNumber: 30,
+                  maxAmplitude: 255,
+                  maxHeight: 80,
+                  minHeight: 10,
+                  animationDuration: const Duration(milliseconds: 100),
+                  barWidth: 5,
+                  barRadius: BorderRadius.circular(5),
+                  barPadding: const EdgeInsets.symmetric(horizontal: 2),
+                  barColor: Colors.green,
+                ),
+              ),
             ],
           ),
         ),
