21f5e40f7ffc04995a047e453da7b5b18f2b1137
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2023-08-17 16:36:58 +0300

    Create app mode picker

---
+++ lib/component/variant_picker.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class VariantPicker<T extends Enum> extends StatelessWidget {
+  final T currentElement;
+  final List<T> variants;
+  final String Function(T) stringBuilder;
+  final void Function(T) onChange;
+  final Color? selectedColor;
+  final double fontSize;
+
+  const VariantPicker({
+    required this.currentElement,
+    required this.onChange,
+    required this.stringBuilder,
+    required this.variants,
+    this.selectedColor,
+    this.fontSize = 16,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: const Color(0xFF1D2630),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: List.generate(
+          variants.length,
+          (index) {
+            final isChosenButton = currentElement == variants[index];
+            final name = stringBuilder(variants[index]);
+
+            return GestureDetector(
+              onTap: () => onChange(variants[index]),
+              child: Container(
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: isChosenButton
+                      ? (selectedColor ?? const Color(0xFF253137))
+                      : const Color(0xFF1D2630),
+                ),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                // margin: const EdgeInsets.symmetric(horizontal: 4),
+                child: Center(
+                  child: Text(
+                    name,
+                    style: const TextStyle(color: Color(0xFFB7BEC4)),
+                    textAlign: TextAlign.center,
+                    maxLines: 1,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,68 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:sound_recognizer/common/enum/app_mode.dart';
+import 'package:sound_recognizer/component/variant_picker.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  AppMode currentMode = AppMode.recognition;
 
   @override
   Widget build(BuildContext context) {
     final screen = MediaQuery.of(context).size;
 
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFF0D0B25), Color(0xFF090B12)],
-            stops: [0.4, 0.8]),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Stack(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    // crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(width: screen.width / 3),
-                      SizedBox(
-                        width: screen.width / 3,
-                        child: Center(
-                          child: FloatingActionButton.large(
-                            onPressed: () {},
-                            backgroundColor: const Color(0xFFE1FE3B),
-                            child: const Icon(
-                              Icons.mic,
-                              color: Colors.black87,
-                            ),
+    return Scaffold(
+      backgroundColor: const Color(0xFF201D1D),
+      body: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              VariantPicker<AppMode>(
+                currentElement: currentMode,
+                variants: AppMode.values,
+                stringBuilder: (mode) {
+                  return (mode == AppMode.recognition)
+                      ? "Recognition"
+                      : "Recording";
+                },
+                onChange: (mode) {
+                  setState(() {
+                    currentMode = mode;
+                  });
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  // crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(width: screen.width / 3),
+                    SizedBox(
+                      width: screen.width / 3,
+                      child: Center(
+                        child: FloatingActionButton.large(
+                          onPressed: () {},
+                          backgroundColor: const Color(0xFFE1FE3B),
+                          child: const Icon(
+                            Icons.mic,
+                            color: Colors.black87,
                           ),
                         ),
                       ),
-                      SizedBox(
-                        width: screen.width / 3,
-                        child: Center(
-                          child: FloatingActionButton.small(
-                            onPressed: () {},
-                            backgroundColor: const Color(0xFFE1FE3B),
-                            child: const Icon(
-                              Icons.swap_horiz,
-                              color: Colors.black87,
-                            ),
+                    ),
+                    SizedBox(
+                      width: screen.width / 3,
+                      child: Center(
+                        child: FloatingActionButton.small(
+                          onPressed: () {},
+                          backgroundColor: const Color(0xFFE1FE3B),
+                          child: const Icon(
+                            Icons.swap_horiz,
+                            color: Colors.black87,
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
